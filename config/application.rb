57ec47eee70a3c6c17870d472d13445a309212e0
masanag <masaki.nagaishi@gmail.com>
2016-03-28 01:49:12 +0900

    modified rails_in_transaction_callback error

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module RailstutorialSample40Slim
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.generators.template_engine = :slim
     config.generators.test_framework = :rspec
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
