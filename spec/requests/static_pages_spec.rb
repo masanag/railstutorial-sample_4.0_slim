3ca4581b8cd4e70203da13224c5f5d404a4a7e08
masanag <masaki.nagaishi@gmail.com>
2016-03-28 14:27:35 +0900

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'StaticPages' do
+  describe 'Home page' do
+    before { visit static_pages_home_path }
+    it "should have the content 'Sample App'" do
+      expect(page).to have_content 'Sample App'
+    end
+
+    it 'should have the right title' do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+  end
+
+  describe 'Help page' do
+    before { visit static_pages_help_path }
+    it "should have the content 'Help'" do
+      expect(page).to have_content 'Help'
+    end
+
+    it 'should have the right title' do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+
+  describe 'About page' do
+    before { visit static_pages_about_path }
+    it "should have the content 'About Us'" do
+      expect(page).to have_content 'About Us'
+    end
+
+    it 'should have the right title' do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+    end
+  end
+end
