6ff3473ef52b25d1d9e8d89e941946a9c9b213fd
masanag <masaki.nagaishi@gmail.com>
2016-04-02 23:42:12 +0900

    done 8.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users;
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   get '/signup', to: 'users#new'
+  get '/signin', to: 'sessions#new'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
