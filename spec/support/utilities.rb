5048497bbd4ffd4651d679b70c480630747400ac
masanag <masaki.nagaishi@gmail.com>
2016-04-05 22:34:47 +0900

    done 9.1

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,6 +6,7 @@ def signin(user, options = {})
     cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
   else
+    visit signin_path
     fill_in 'Email', with: user.email.upcase
     fill_in 'Password', with: user.password
     click_button 'Sign in'
