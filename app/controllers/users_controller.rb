ecb04ece400800da3a1553b285e20b611afffbed
masanag <masaki.nagaishi@gmail.com>
2016-04-08 09:48:38 +0900

    done 10.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,13 +59,6 @@ class UsersController < ApplicationController
 
   # Before actions
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: 'Please sign in.'
-    end
-  end
-
   def no_signed_in_user
     if signed_in?
       redirect_to root_path, notice: 'Could not respond to your access'
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -107,6 +107,19 @@ describe 'AuthenticationPages' do
           end
         end
       end
+
+      describe 'in the Microposts controller' do
+        describe 'submitting to the create action' do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe 'submitting to the destroy action' do
+          before { delete micropost_path(create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+
+        end
+      end
     end
 
     describe 'as valid user' do
